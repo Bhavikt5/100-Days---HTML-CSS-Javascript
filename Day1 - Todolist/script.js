11923daab56ae62e1d259e97317edf266c0d09a7
bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2023-04-05 16:17:47 +0530

    todo list app completed

--- Day1 - Todolist/script.js
+++ Day1 - Todolist/script.js
@@ -1,60 +1,43 @@
-const formSubmit = document.getElementById("form");
 const input = document.getElementById("input");
-const postTask = document.getElementById("getLists");
-const postedTask = document.getElementById("postedTask");
-const message = document.getElementById("message");
-const pendingDiv = document.querySelector(".pendingDiv");
+const form = document.getElementById("form");
+const content = document.getElementById("content");
 
-formSubmit.addEventListener("submit", submitTask);
+form.addEventListener("submit", submitForm);
 
-console.log("App Started");
-
-function submitTask(e) {
+function submitForm(e) {
   e.preventDefault();
 
-  const inputValue = input.value;
-  taskArray.push(inputValue);
+  if (input.value === "") {
+    alert("Enter ToDo!!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    li.classList.add("lists");
+    content.appendChild(li);
+    let trash = document.createElement("button");
+    const trashClass = ["trash", "fa-solid", "fa-trash"];
+    trash.classList.add(...trashClass);
+    li.appendChild(trash);
 
-  postTask.innerHTML += `<div class="lists">
-                            <p>${inputValue}</p>
-                            <button class="trash" type="menu">
-                                <i class="fa-solid fa-trash"></i>
-                            </button>
-                        </div> `;
+    saveData();
 
-  console.log(taskArray);
-
-  if (taskArray.length > 0) {
-    message.innerHTML = `You have ${taskArray.length} pending tasks`;
-    pendingDiv.style.display = "flex";
+    input.value = "";
   }
-  console.log(deleteTasks);
-
-  console.log(postTask);
 }
 
-// const taskArray = [];
-
-const taskArray = [];
-
-const removeTask = () => {
-  for (let i = 0; i < taskArray.length; i++) {
-    deleteTasks[i].addEventListener("click", (i) => {
-      console.log(i);
-    });
+content.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentNode.remove();
+    saveData();
   }
-};
-
-const lists = document.querySelectorAll(".lists");
-const deleteTasks = document.getElementsByClassName("trash");
-
-deleteTasks.forEach((i) => {
-  i.addEventListener("click", () => {
-    console.log("click");
-  });
 });
-// for (let i = 0; i < deleteTasks.length; i++) {
-//   deleteTasks[i].addEventListener("click", () => {
-//     console.log("delete task");
-//   });
-// }
+
+function saveData() {
+  localStorage.setItem("data", content.innerHTML);
+}
+
+function showData() {
+  content.innerHTML = localStorage.getItem("data");
+}
+
+showData();
